3ee17c57a5911b390368feecd42867955399d468
Brice Bentler <server4001@gmail.com>
2017-05-28 10:51:33 -0700

    Changes logging

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ exports.handler = (event, context, callback) => {
     const config = require('./config');
     const message = event.Records[0].Sns.Message;
     const eventName = message.Event;
-    const instanceId = message.EC2InstanceId;
-    const availabilityZone = message.Details['Availability Zone'];
-    let newEventName;
 
     if (eventName === 'autoscaling:EC2_INSTANCE_LAUNCH') {
-        newEventName = 'SCALE OUT';
+        // const availabilityZone = message.Details['Availability Zone'];;
+        console.log({ id: message.EC2InstanceId, az: message.Details, event: 'SCALE OUT' });
     } else if (eventName === 'autoscaling:EC2_INSTANCE_TERMINATE') {
-        newEventName = 'SCALE IN';
+        // const availabilityZone = message.Details['Availability Zone'];
+        console.log({ id: message.EC2InstanceId, az: message.Details, event: 'SCALE IN' });
     }
 
-    console.log({ id: instanceId, az: availabilityZone, event: newEventName });
     callback(null);
     // callback({}); // error.
 };
